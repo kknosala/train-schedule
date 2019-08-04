4a46e755737c3f0de3eb433158809f4cecffb5ac
KSK <kurtknosala@gmail.com>
2019-08-04 13:58:48 -0600

    firebase data storage setup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
     
+    var database = firebase.database();
 
+    function storeData(){
 
+        var trainInput = $('#train-input').val().trim();
+        var desinationInput = $('#destination-input').val().trim();
+        var firstInput = $('#time-input').val();
+        var frequencyInput = $('#frequency-input').val().trim();
 
+        database.ref().push({
+            trainName: trainInput,
+            destination: desinationInput,
+            startTime: firstInput,
+            frequency: frequencyInput,
+        })
 
+    }
+
+    $('#schedule-submit').click(function(){
+
+        event.preventDefault();
+        storeData();
+    })
 
 
 
